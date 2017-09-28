a7fb71fac1262ac6f5fcfe1d45e1365093fcfee3
shusiniski <anarak@code.edu.az>
2017-09-28 18:05:22 +0400

    kecdiyimiz teze

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// obj={
+// 	a:function() {
+// 		console.log("heci")
+// 		return this;
+
+
+// 	},
+// 	b:function() {
+// 		console.log("mesedi")
+// 		return this;
+// 	}
+// }
+
+// obj.a().b()
+
+
+// $("li").click(function() {
+// 	$("li").removeClass("active");
+// 	$(this).addClass("active");
+// 	console.log()
+// })
+
+
+// $(window).on('mousemove',function(event){
+
+// 	$(".main").css({
+// 		left:event.pageX,
+// 		top:event.pageY,
+// 	})
+// });
+
